2ded2839624bca2ddebf813354949addbc7da9fc
rodrigolck <rodrigolck@gmail.com>
2017-05-06 22:46:35 -0300

    Create rails_install.sh
    Created the script that needs to be installed in the VM, when vagrant up is ran for the first time

---
+++ rails_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install dependencies
+sudo apt-get update -y
+sudo apt-get install -y curl libfontconfig
+
+# Install RVM with ruby and rails
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable --ruby=2.2.2
+echo "gem: --no-document" > /home/vagrant/.gemrc
+echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"' >> /home/vagrant/.bashrc
+source /home/vagrant/.rvm/scripts/rvm
+rvm 2.2.2
+gem install rails -fv 4.2.3
+gem install bundler
+
+# Install PhantomJS for testing
+export PHANTOM_JS="phantomjs-1.9.8-linux-x86_64"
+cd /tmp
+curl -L https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2 | tar xvjf -
+sudo mv $PHANTOM_JS /usr/local/share
+sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin
+
+#Install MongoDB
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64 ] http://repo.mongodb.org/apt/ubuntu precise/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt-get update -y
+sudo apt-get install -y mongodb-org
